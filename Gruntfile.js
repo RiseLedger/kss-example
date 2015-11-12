590edfbdb94affb24b988fac75a7a1a191e2afcc
Rise <riseledger@gmail.com>
2015-11-12 13:47:18 +0200

    Add task compiler for kss

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,20 +10,9 @@ module.exports = function(grunt) {
                     'assets/dist/main.css': 'assets/src/main.scss'
                 }
             }
-        },
-
-        copy: {
-            dist: {
-                files: {
-                    'styleguide/css/main.css': 'assets/dist/main.css'
-                }
-            }
-        },
+        }
     });
 
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-copy');
-
-    grunt.registerTask('kss', ['copy']);
-    grunt.registerTask('default', ['sass', 'kss']);
+    grunt.registerTask('default', ['sass']);
 };
--- assets/src/styleguide.md
+++ assets/src/styleguide.md
@@ -3,3 +3,7 @@
 This is the description. Visit [Markdown Syntax](https://help.github.com/articles/markdown-basics/) for more info how to write this file
 
 > Some slogan
+
+Useful links for KSS:
+- [Quick Start Guide](https://github.com/kss-node/kss-node/wiki/quick-start-guide)
+- [Writting Comments](https://github.com/kss-node/kss/blob/spec/SPEC.md)
